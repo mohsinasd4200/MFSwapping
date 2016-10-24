273ec2d2ee0cd3354eefefb638009edfabe23c6d
Mohsin Farooqui <mohsin.farooqui4@gmail.com>
2016-10-24 13:10:48 +0530

    Awesome

--- MFSwapping/main.m
+++ MFSwapping/main.m
@@ -7,11 +7,36 @@
 //
 
 #import <Foundation/Foundation.h>
+void swap(int *,int *);
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        
+        
+    
+            int number1,number2;
+            printf("Enter the two numbers\n");
+            
+            scanf("%d%d",&number1,&number2);
+            
+            swap(&number1,&number2);
+            printf("Now the two numbers are:\n%d\n %d\n",number1,number2);
+            
+        
+        
+        
+        
+        
     }
     return 0;
+}
+
+void swap(int *a,int *b)
+{
+    int temp;
+    temp=*a;
+    *a=*b;
+    *b=temp;
+    
 }
